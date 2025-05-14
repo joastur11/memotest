24ab5cbcd1c54673219ae24a6911b4b6e28cfb07
joastur11 <joaquinstur@gmail.com>
2025-05-13 22:31:36 -0300

    Crea el codigo base
    Cree el codigo para que cada par de cuadros se pinte de un color al azar, sin repetirse.

---
+++ index.js
@@ -0,0 +1,52 @@
+// boton empezar juego, cambiar mensaje de bienvenida por "el juego ha comenzado"
+// que los cuadros (en pares) se pinten de un color random sin que se vea 
+// revelar el color cuando se clickee un cuadro, mantenerlo hasta que se clickee otro
+// color en clase oculto, que se revele cuando se haga click
+// si los colores son iguales, poner opacidad de los cuadros en 0 y bloquear el input de esos cuadros.
+// si son distintos, los cuadros vuelven al color de fondo
+// ir contando el numero de intentos cada dos clicks 
+// el juego termina cuando desaparecen todos los cuadros, no se puede perder
+// el boton de empezar se transforma en boton de reinicio (no cambia la funcionalidad (creo) solo la palabra)
+
+let intento = 0
+
+document.querySelector('button[type=button]').onclick = comenzarJuego
+
+function comenzarJuego (){
+    reiniciarIntento ()
+    manejarRonda ()
+}
+
+function reiniciarIntento (){
+    ronda = 0
+}
+
+function manejarRonda (){
+    //actualizar estado
+    pintarCuadros()
+}
+
+function pintarCuadros (){
+    const $cuadros = document.querySelectorAll('.cuadro');
+
+    let indices = []
+    for (i=0; i<$cuadros.length; i++){
+    indices.push (i)
+    };
+
+    indices.sort(() => 0.5 - Math.random())
+    
+    let pares = [];
+    for (let i=0; i<indices.length; i+=2) {
+        pares.push([indices[i], indices[i + 1]])
+    }
+    for (let i=0; i<pares.length; i++){
+        const [a, b] = pares[i];
+        const colores = ['red', 'green', 'blue', 'orange', 'purple', 'pink', 'yellow', 'brown'];
+        const color = colores[i]
+        $cuadros[a].style.backgroundColor = color
+        $cuadros[b].style.backgroundColor = color
+    }
+}
+
+
